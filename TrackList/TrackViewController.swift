294c0608151f6437e6d2dd5c439b3ca4ee40bbdb
Dmitry Yavorsky <Yavorskyd@yandex.ru>
2022-10-25 11:58:27 +0300

    create app

---
+++ TrackList/TrackViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TrackList
+//
+//  Created by Yavorsky on 24.10.2022.
+//
+
+import UIKit
+
+class TrackViewController: UITableViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
+extension TrackViewController {
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	return 8
+}
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+	let cell = tableView.dequeueReusableCell(withIdentifier: "cellio", for: indexPath)
+	cell.textLabel?.text = "Cell Index: \(indexPath.row)"
+	return cell
+}
+}
+
+
